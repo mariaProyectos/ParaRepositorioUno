084256d1f26dd4014e76eda0c4279fbf32ab2408
maria <maria.cabeza.castillo@gmail.com>
2015-02-12 13:42:29 +0100

    He creado un interface con metodos lo dice jose

---
+++ ParaRepositorioEnredar/src/EstoEsUnaPrueba/PruebaInterface.java
@@ -0,0 +1,15 @@
+package EstoEsUnaPrueba;
+
+public interface PruebaInterface {
+public static int aquiSeSuma(int numero){
+	int suma=0;
+	suma=10+numero;
+	return numero;
+}
+public static int aquiSeResta(int numero){
+	int resta=0;
+	resta=10-numero;
+	return numero;
+	
+}
+}
