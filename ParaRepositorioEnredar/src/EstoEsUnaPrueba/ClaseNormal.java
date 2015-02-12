0f1d2135d6699a229054edfd4748d01f945f5a6f
maria <maria.cabeza.castillo@gmail.com>
2015-02-12 13:54:00 +0100

    cambios de variables

--- ParaRepositorioEnredar/src/EstoEsUnaPrueba/ClaseNormal.java
+++ ParaRepositorioEnredar/src/EstoEsUnaPrueba/ClaseNormal.java
@@ -2,9 +2,9 @@ package EstoEsUnaPrueba;
 
 public class ClaseNormal implements PruebaInterface{
 	public static int multiplicamos() {
-		int numero = 5;
 		int resultado = 0;
-		return resultado = numero * 2;
+		int numero2=0;
+		return resultado = numero2 * 2;
 
 	}
 }
